4ef2b1290a87cb0a64a33fbff5becfd875475b65
Administrator <d.kozicki83@gmail.com>
2016-12-12 16:28:57 +0100

    initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Created by Administrator on 2016-12-12.
+ */
+public class SimpleTest {
+
+    @Test
+    public void shouldAddOneTest(){
+        assertThat(1+1, equalTo(2));
+        assertNotEquals(1+1, equalTo(3));
+    }
+}

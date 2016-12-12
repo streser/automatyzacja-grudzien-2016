05ad4fde463657e04cf996d1331b9a0bd398971e
wojtys <tomasz.wojtys@tomtom.com>
2016-12-12 16:28:58 +0100

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+
+public class SimpleTest {
+
+    @Test
+    public void shouldAddOneToOne() {
+       assertThat(1 + 1, equalTo(2));
+
+    }
+
+}

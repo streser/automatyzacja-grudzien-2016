494761e36ba9d59b9114ee22c6664578ec36b42a
piotrheliosz <piotr.heliosz@gmail.com>
2016-12-13 12:31:39 +0100

    refactoring

---
+++ src/test/java/TestScenario.java
@@ -0,0 +1,78 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
+
+public class TestScenario {
+    protected WebDriver driver;
+    protected String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        baseUrl = "https://szkolenieautoamatyzacjatech.wordpress.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    protected void tryLogin() {
+        InsertText(By.id("user_login"), "Warsztatautomatyzacja");
+        InsertText(By.id("user_pass"), "notsosimplepass123");
+        driver.findElement(By.id("wp-submit")).click();
+    }
+
+    private void InsertText(By by, String text) {
+        driver.findElement(by).clear();
+        driver.findElement(by).sendKeys(text);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.close();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    protected boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}

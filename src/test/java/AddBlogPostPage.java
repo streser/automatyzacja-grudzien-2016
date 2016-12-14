99db35b0ab571438e7d51824a8c7e490897f18be
Lidia <l.k@pl.pl>
2016-12-14 14:37:52 +0100

    initial commit

--- src/test/java/AddBlogPostPage.java
+++ src/test/java/AddBlogPostPage.java
@@ -1,28 +1,27 @@
 import org.junit.Test;
 import pageObjectPattern.*;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Administrator on 2016-12-13.
  */
 public class AddBlogPostPage extends TestScenario {
 
     @Test
-    public void should(){
+    public void should() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
-        MainPage mainPage = loginPage.logIn (USER_NAME, PASSWORD);
+        MainPage mainPage = loginPage.logIn(USER_NAME, PASSWORD);
 
         PostPage postPage = mainPage.goToPostPage();
         AddNewPostPage addNewPost = postPage.goToNewPostPage();
-        addNewPost.publishNewPost ("title", "content");
+        addNewPost.publishNewPost("LKtitle", "LKcontent");
         mainPage.logOut();
-
-        HomePage homePage = new HomePage(driver);
-        homePage.open();
- //       assertTrue(homePage.isPostPublished("title"));
+        HomePage homePage = loginPage.backToBlog();
+        assertTrue(homePage.isPostPublished("LKtitle"));
 
 
     }
 
-   // private void assertTrue(void title) {
-   // }
+    // private void assertTrue(void title) {}
 }

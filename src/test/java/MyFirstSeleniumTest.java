3a519c8e0e2e0664db0e411ea805d0beb2c51468
Lidia <l.k@pl.pl>
2016-12-14 14:49:35 +0100

    initial commit

--- src/test/java/MyFirstSeleniumTest.java
+++ src/test/java/MyFirstSeleniumTest.java
@@ -6,31 +6,31 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.*;
 
-
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.fail;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-
-public class MyFirstSeleniumTest extends TestScenario {
-
-
-    @Test
-
-    public void test1java() throws Exception {
-        // Given - przygotowanie do testu
-        open(BaseUrl + "/wp-admin/");
-        // When - logowanie do stronki
-        tryLogIn(USER_NAME, PASSWORD);
-        // Then - asserts i wylogowanie się
-        tryLogOut();
-
-    }
-
-
-}
+//
+//import static junit.framework.TestCase.assertTrue;
+//import static org.junit.Assert.fail;
+//
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//
+//public class MyFirstSeleniumTest extends TestScenario {
+//
+//
+//    @Test
+//
+//    public void test1java() throws Exception {
+//        // Given - przygotowanie do testu
+//        open(BaseUrl + "/wp-admin/");
+//        // When - logowanie do stronki
+//        tryLogIn(USER_NAME, PASSWORD);
+//        // Then - asserts i wylogowanie się
+//        tryLogOut();
+//
+//    }
+//
+//
+//}
 
 
